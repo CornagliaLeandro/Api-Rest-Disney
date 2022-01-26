33375068aaf4342a9fbc95114a551753b5558d3d
Leandro Cornaglia <cornaglia1987@gmail.com>
2022-01-25 22:29:39 -0300

    Puntos de personajes Terminados(falta pulir detalles)

--- src/main/java/com/disney_api/app/repositorios/PersonajeRepositorio.java
+++ src/main/java/com/disney_api/app/repositorios/PersonajeRepositorio.java
@@ -1,11 +1,27 @@
 package com.disney_api.app.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.disney_api.app.entidades.Personaje;
 
 @Repository
 public interface PersonajeRepositorio extends JpaRepository<Personaje, Long> {
+	
+	@Query("SELECT p FROM Personaje p WHERE p.nombre =:name")
+	public List<Personaje> personajeNombre(@Param ("name") String name);
+
+    @Query("SELECT p FROM Personaje p WHERE p.aparicion = :aparicion")
+    public List<Personaje> personajeAparicion(@Param("aparicion") String aparicion);
+    
+    @Query("SELECT p FROM Personaje p WHERE edad = :age")
+    public List<Personaje> personajeEdad(@Param("age") Integer edad);
+
+    @Query("SELECT p FROM Personaje p WHERE peso = :weight")
+    public List<Personaje> personajePeso(@Param("weight") Double peso);
 
 }
--- src/main/java/com/disney_api/app/servicios/PersonajeServicio.java
+++ src/main/java/com/disney_api/app/servicios/PersonajeServicio.java
@@ -1,5 +1,9 @@
 package com.disney_api.app.servicios;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +15,103 @@ public class PersonajeServicio {
 
 	@Autowired
 	PersonajeRepositorio personajeRepositorio;
+	
+	public List<Personaje> listadoPersonajes(){
+		List<Personaje> listaCompleta = personajeRepositorio.findAll();
+		List<Personaje> listaMostrar = new ArrayList<Personaje>();
+		
+		for (Personaje personaje : listaCompleta) {
+			Personaje p = new Personaje();
+			p.setImagen(personaje.getImagen());
+			p.setNombre(personaje.getNombre());
+			listaMostrar.add(p);
+			
+		}
+		
+		return listaMostrar;
+	}
 
-	public Personaje crearPersonaje(Personaje personaje) {
-		return personajeRepositorio.save(personaje);
+	public Personaje detallePersonaje(Long id) throws Exception {
+		
+		try {
+			return personajeRepositorio.findById(id).get();
+		} catch (Exception e) {
+			throw new Exception("Error al detallar personaje");
+		}
+		
+	}
+
+	public Personaje crearPersonaje(Personaje personaje) throws Exception {
+
+		try {
+			return personajeRepositorio.save(personaje);
+		} catch (Exception e) {
+			throw new Exception("Error al crear el personaje");
+		}
+
+	}
+
+	public Personaje modificar(Personaje personaje) throws Exception {
+
+		try {
+			if (personajeRepositorio.findById(personaje.getId()).isPresent()) {
+				return personajeRepositorio.save(personaje);
+			
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			throw new Exception("Error al modificar Personaje");
+		
+		}
+	
 	}
 	
+	public void eliminar(Long id) throws Exception {
+		try {
+			personajeRepositorio.deleteById(id);
+		} catch (Exception e) {
+			throw new Exception ("Error al eliminar un Personaje");
+		}
+		
+	}
+	
+	public List<Personaje> personajeNombre(String nombre) throws Exception{
+		try {
+			return personajeRepositorio.personajeNombre(nombre);
+		} catch (Exception e) {
+			throw new Exception("Error al buscar por nombre");
+		}
+		
+	}
+	
+	public List<Personaje> personajeAparicion(String aparicion) throws Exception{
+		try {
+			return personajeRepositorio.personajeAparicion(aparicion);
+		} catch (Exception e) {
+			throw new Exception("Error al buscar por Pelicula");
+		}
+        
+    } 
+	
+	public List<Personaje> personajeEdad(Integer edad) throws Exception{
+		try {
+			return personajeRepositorio.personajeEdad(edad);
+		} catch (Exception e) {
+			throw new Exception("Error al buscar por Edad");
+		}
+		
+        
+    } 
+	
+	public List<Personaje> personajePeso(Double peso) throws Exception{
+		try {
+			return personajeRepositorio.personajePeso(peso);
+		} catch (Exception e) {
+			throw new Exception("Error al buscar por Peso");
+		}
+        
+    } 
+
+
 }
